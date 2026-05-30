1dd8145447486736a9afe86c0b90eb0c6af47624
DarkTrade Dev <admin@darktrade.app>
2026-05-30 22:11:16 +0800

    feat: register WatchlistService in provider tree

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:dark_trade_app/app/app.dart';
 import 'package:dark_trade_app/data/local/hive_service.dart';
 import 'package:dark_trade_app/data/repositories/career_repository.dart';
 import 'package:dark_trade_app/data/repositories/trade_history_repository.dart';
+import 'package:dark_trade_app/data/repositories/watchlist_repository.dart';
 import 'package:dark_trade_app/data/remote/supabase_client.dart';
 import 'package:dark_trade_app/domain/services/a_share_service.dart';
 import 'package:dark_trade_app/domain/services/auth_service.dart';
@@ -9,6 +10,7 @@ import 'package:dark_trade_app/domain/services/career_service.dart';
 import 'package:dark_trade_app/domain/services/portfolio_service.dart';
 import 'package:dark_trade_app/domain/services/trade_history_service.dart';
 import 'package:dark_trade_app/domain/services/trade_selection_service.dart';
+import 'package:dark_trade_app/domain/services/watchlist_service.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
@@ -32,12 +34,16 @@ void main() async {
   final portfolio = PortfolioService()..seedDemo();
   final tradeSelection = TradeSelectionService();
 
+  final watchlistRepo = HiveWatchlistRepo();
+  final watchlistService = WatchlistService(localRepo: watchlistRepo)..load();
+
   runApp(
     MultiProvider(
       providers: [
         ChangeNotifierProvider.value(value: aShare),
         ChangeNotifierProvider.value(value: portfolio),
         ChangeNotifierProvider.value(value: tradeSelection),
+        ChangeNotifierProvider.value(value: watchlistService),
         ChangeNotifierProvider.value(value: careerService),
         ChangeNotifierProvider.value(value: tradeHistory),
         ChangeNotifierProvider(create: (_) => AuthService()),
